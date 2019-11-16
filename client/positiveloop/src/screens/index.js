b84086d71d6b7ae16fda9f7328dc35eb625f1adf
jonirautiainen <joni.rautiainen@aalto.fi>
2019-11-16 20:05:19 +0200

    Initial me

--- client/positiveloop/src/screens/index.js
+++ client/positiveloop/src/screens/index.js
@@ -6,6 +6,7 @@ import { createStore } from 'redux';
 import { test } from "../store/reducers";
 import Test from './Test';
 import Record from './Record';
+import Me from './Me';
 
 let store;
 function ReduxProvider(Component) {
@@ -25,5 +26,5 @@ export default () => {
   Navigation.registerComponent('Listen', () => ReduxProvider(Test), () => Test);
   Navigation.registerComponent('Record', () => ReduxProvider(Record), () => Record);
   Navigation.registerComponent('Saved', () => ReduxProvider(Test), () => Test);
-  Navigation.registerComponent('Me', () => ReduxProvider(Test), () => Test);
+  Navigation.registerComponent('Me', () => ReduxProvider(Me), () => Me);
 }
