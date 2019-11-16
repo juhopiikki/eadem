814d23120fd03f67828aaa514a9e3f97012aa0c3
Olli Piuhola <olli.piuhola@aalto.fi>
2019-11-16 12:34:15 +0200

    Register screen in index

--- client/positiveloop/index.js
+++ client/positiveloop/index.js
@@ -1,4 +1,7 @@
 import {Navigation} from 'react-native-navigation';
+import App from './App';
+
+Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
 
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
