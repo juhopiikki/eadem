80e0cc60410532fd8650b310437e59d9fba98a80
Olli Piuhola <olli.piuhola@aalto.fi>
2019-11-16 14:21:16 +0200

    Move initilization logic under src

---
+++ client/positiveloop/src/index.js
@@ -0,0 +1,94 @@
+import {Navigation} from 'react-native-navigation';
+import registerScreens from './screens'
+
+function createScreenTree() {
+    Navigation.events().registerAppLaunchedListener(() => {
+        Navigation.setRoot({
+            root: {
+                bottomTabs: {
+                    children: [{
+                        stack: {
+                            children: [{
+                                component: {
+                                    name: 'Listen',
+                                    passProps: {
+                                        text: 'This is tab 1'
+                                    }
+                                }
+                            }],
+                            options: {
+                                bottomTab: {
+                                    icon: require('./assets/test.png'),
+                                    text: 'Listen',
+                                    testID: 'FIRST_TAB_BAR_BUTTON'
+                                }
+                            }
+                        }
+                    },
+                        {
+                            stack: {
+                                children: [{
+                                    component: {
+                                        name: 'Record',
+                                        passProps: {
+                                            text: 'This is tab 1'
+                                        }
+                                    }
+                                }],
+                                options: {
+                                    bottomTab: {
+                                        icon: require('./assets/test.png'),
+                                        text: 'Record',
+                                        testID: 'FIRST_TAB_BAR_BUTTON'
+                                    }
+                                }
+                            }
+                        }, {
+                            stack: {
+                                children: [{
+                                    component: {
+                                        name: 'Saved',
+                                        passProps: {
+                                            text: 'This is tab 1'
+                                        }
+                                    }
+                                }],
+                                options: {
+                                    bottomTab: {
+                                        icon: require('./assets/test.png'),
+                                        text: 'Saved',
+                                        testID: 'FIRST_TAB_BAR_BUTTON'
+                                    }
+                                }
+                            }
+                        }, {
+                            stack: {
+                                children: [{
+                                    component: {
+                                        name: 'Me',
+                                        passProps: {
+                                            text: 'This is tab 1'
+                                        }
+                                    }
+                                }],
+                                options: {
+                                    bottomTab: {
+                                        icon: require('./assets/test.png'),
+                                        text: 'Me',
+                                        testID: 'FIRST_TAB_BAR_BUTTON'
+                                    }
+                                }
+                            }
+                        },
+                    ]
+                }
+            }
+        });
+    });
+}
+
+export function initializeApp () {
+    registerScreens();
+    createScreenTree();
+}
+
