377ce8fe47c66cc3411409bd4d5b2d411afd65db
Olli Piuhola <olli.piuhola@aalto.fi>
2019-11-16 14:12:11 +0200

    Fix registerScreens

--- client/positiveloop/index.js
+++ client/positiveloop/index.js
@@ -1,7 +1,7 @@
 import {Navigation} from 'react-native-navigation';
 import registerScreens from './src/screens'
 
-registerScreens()
+registerScreens();
 
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
