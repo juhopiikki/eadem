5e5d29916c9df17cc73649ccc39606852238e74c
jonirautiainen <joni.rautiainen@aalto.fi>
2019-11-17 04:33:02 +0200

    tuttifrutti tuli

--- client/positiveloop/src/assets/colors.js
+++ client/positiveloop/src/assets/colors.js
@@ -2,7 +2,7 @@ const colors = {
     inactiveTab: 'rgb(100,100,100)',
     activeTab: 'rgb(0,0,255)',
     background: 'rgb(255,255,255)',
-    buttonDefault: 'rgb(0,0,255)',
+    buttonDefault: '#5300DA',
 };
 
 export default colors;
--- client/positiveloop/src/screens/Record/index.js
+++ client/positiveloop/src/screens/Record/index.js
@@ -1,67 +1,317 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import { Button, IconButton, TextInput } from 'react-native-paper';
+import { ProgressBar, Button, IconButton, TouchableRipple } from 'react-native-paper';
 import colors from "../../assets/colors";
 import AudioRecorder from '../../utils/AudioRecorder';
 
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
 
 export default Record = () => {
-    const audioRecorder: AudioRecorder = new AudioRecorder();
+    const audioRecorder = new AudioRecorder();
+    const [title, setTitle] = React.useState('');
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [isRecording, setIsRecording] = useState(false);
+    const [hasRecorded, sethasRecorded] = useState(false);
+    const [timeRecorded, setTimeRecorded] = useState(0);
+
+
+    useInterval(() => {
+      setTimeRecorded(timeRecorded + 1);
+    }, isRecording ? 1000 : null);
+
+    const play = () => {
+      setIsPlaying(true)
+    }
+
+    const pause = () => {
+      setIsPlaying(false)
+    }
+
+    const record = () => {
+      setIsRecording(true)
+      // audioRecorder.record()
+    }
+    
+    const stopRecord = () => {
+      setIsRecording(false)
+      sethasRecorded(true)
+      setTimeRecorded(0)
+      // audioRecorder.record()
+    }
+
+    const restartRecording = () => {
+      setIsRecording(false)
+      sethasRecorded(false)
+      setTimeRecorded(0)
+      setIsPlaying(false)
+      // audioRecorder.record()
+    }
 
     return (
         <>
-            <KeyboardAwareScrollView bounces={false} contentContainerStyle={styles.main} keyboardShouldPersistTaps='handled'>
-                <View style={styles.body}>
+            <View style={{
+              flex: 1,
+              flexDirection: 'column', 
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
 
-                    <View style={{ flex: 1, flexDirection: 'row', marginBottom: 10 }}>
-                        <TextInput style={{ backgroundColor: colors.background }}></TextInput>
-                    </View>
-
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                        <IconButton
-                            icon="record"
-                            size={60}
-                            style={{ borderWidth: 4 }}
-                            onPress={() => audioRecorder.record()}
-                        />
-                        <Text style={{ fontSize: 22 }}>Record</Text>
-                    </View>
-
-                    <View style={{ flex: 1, justifyContent: 'flex-end' }}>
-                        <Button
-                            mode="contained"
-                            color={colors.buttonDefault}
-                            uppercase={false}
-                            style={{ marginBottom: 60 }}
-                            contentStyle={{ paddingHorizontal: 36, paddingVertical: 8 }}
-                            labelStyle={{ fontSize: 20 }}
-                            onPress={() => console.log('Share')}
-                        >
-                            Share
-                        </Button>
-                    </View>
+                <View style={{ 
+                  height: 80,
+                  width: '100%',
+                  flexDirection: 'row', 
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  paddingHorizontal: 30,
+                  // backgroundColor: 'skyblue'
+                }}>
+                  {hasRecorded &&
+                  <>
+                  <IconButton
+                      icon={require('../../assets/images/restart.png')}
+                      color={'black'}
+                      size={30}
+                      onPress={() => restartRecording()}
+                  />
+                  <Text style={[styles.tinyLabel, {
+                      bottom: 0
+                  }]}>
+                      Restart recording
+                  </Text>
+                  </>}
                 </View>
-            </KeyboardAwareScrollView>
+
+                <View style={{ 
+                  flex: 1,
+                  flexDirection: 'row', 
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  marginBottom: 10,
+                  paddingHorizontal: 30,
+                  // backgroundColor: 'skyblue'
+                }}>
+                    <TextInput 
+                      underlineColorAndroid="transparent" 
+                      style={{
+                        backgroundColor: colors.background,
+                        fontFamily: 'NunitoSans_bold',
+                        width: '100%',
+                        borderBottomColor: '#000000', 
+                        borderBottomWidth: 1,
+                        backgroundColor: colors.backgroundColor,
+                        textAlign: 'center',
+                        fontSize: 30, 
+                      }}
+                      maxLength={50}
+                      multiline
+                      placeholder="Title"
+                      // placeholderTextColor="white"
+                      onChangeText={text => {
+                        if(text.length <= 50){
+                          setTitle(text)
+                        }
+                      }}
+                      value={title}
+                    />
+                </View>
+
+
+
+
+                <View style={{
+                    // backgroundColor: 'pink',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
+                    
+                    <View style={{
+                        // backgroundColor: 'pink',
+                        height: 40,
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}>
+                      {hasRecorded ?
+                      <ProgressBar
+                        visible={true}
+                        progress={0.5}
+                        color={'#F83E81'}
+                        style={{ height: 8, width: 300 }}
+                      />
+                      :
+                      <Text style={{
+                        fontFamily: 'NunitoSans_bold',
+                        fontSize: 18
+                      }}>
+                          {isRecording ? `${('0' + (Math.floor(timeRecorded/60))).slice(-2)}:${('0' + (timeRecorded%60)).slice(-2)}` : ''}
+                      </Text>
+                      }
+                    </View>
+
+                    
+                    {!hasRecorded ?
+                    <View style={{
+                        // backgroundColor: 'pink',
+                        height: 100,
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}>
+                      {isRecording ?
+                          <TouchableRipple
+                              borderless={true}
+                              onPress={() => stopRecord()}
+                          >
+                              <Image style={{ width: 75, height: 75 }} source={require('../../assets/images/stop-record.png')}></Image>
+                          </TouchableRipple>
+                          :
+                          <TouchableRipple
+                              borderless={true}
+                              onPress={() => record()}
+                          >
+                              <Image style={{ width: 75, height: 75 }} source={require('../../assets/images/record.png')}></Image>
+                          </TouchableRipple>
+                      }
+
+                      <Text style={styles.tinyLabel}>
+                          {isRecording ? 'Stop recording' : 'Start recording'}
+                      </Text>
+                    </View>
+
+                      :
+
+                    <View style={{
+                        // backgroundColor: 'pink',
+                        height: 100,
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        paddingHorizontal: 10
+                    }}>
+                      <View style={{
+                            // backgroundColor: 'pink',
+                            flex: 1,
+                            flexDirection: 'row',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }}>
+                          <IconButton
+                                icon={require('../../assets/images/backward.png')}
+                                color={'black'}
+                                size={40}
+                                onPress={() => console.log('Pressed')}
+                            />
+                        </View>
+
+                        <View style={{
+                            // backgroundColor: 'pink',
+                            flex: 1,
+                            flexDirection: 'row',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }}>
+                          {isPlaying ?
+                          <TouchableRipple
+                              borderless={true}
+                              onPress={() => pause()}
+                          >
+                              <Image style={{ width: 75, height: 75 }} source={require('../../assets/images/pause.png')}></Image>
+                          </TouchableRipple>
+                          :
+                          <TouchableRipple
+                              borderless={true}
+                              onPress={() => play()}
+                          >
+                              <Image style={{ width: 75, height: 75 }} source={require('../../assets/images/play.png')}></Image>
+                          </TouchableRipple>
+                          }
+
+                          <Text style={styles.tinyLabel}>
+                              {isRecording ? 'Play' : 'Play'}
+                          </Text>
+                        </View>
+                      
+
+                      <View style={{
+                            // backgroundColor: 'pink',
+                            flex: 1,
+                            flexDirection: 'row',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }}>
+                          <IconButton
+                                icon={require('../../assets/images/forward.png')}
+                                color={'black'}
+                                size={40}
+                                onPress={() => console.log('Pressed')}
+                            />
+                        </View>
+                    </View>
+                  }
+
+                </View>
+
+
+
+
+
+
+
+
+                <View style={{ 
+                  flex: 1, 
+                  justifyContent: 'flex-end' 
+                }}>
+                    <Button
+                        mode="contained"
+                        color={colors.buttonDefault}
+                        uppercase={false}
+                        style={{ 
+                          marginBottom: 60,
+                          borderRadius: 6 
+                        }}
+                        contentStyle={{ paddingHorizontal: 36, paddingVertical: 8 }}
+                        labelStyle={{ 
+                          fontSize: 22,
+                          fontFamily: 'NunitoSans_bold', 
+                        }}
+                        onPress={() => console.log('Share')}
+                    >
+                        Share
+                    </Button>
+                </View>
+            </View>
         </>
     );
 };
 
 const styles = StyleSheet.create({
-    main: {
-        flex: 1,
-        backgroundColor: 'white',
-    },
-    body: {
-        flexDirection: 'column',
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: 'white',
-    },
-    bottomContent: {
-        flex: 1,
-        justifyContent: 'flex-end'
-    }
+  tinyLabel: {
+      fontFamily: 'NunitoSans',
+      fontSize: 12,
+      position: 'absolute',
+      bottom: -20
+  }
 });
 
 
