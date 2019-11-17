93c923d7b18fa5a989d74fd3993e2871759879d8
Mikko Etelätalo <miksumortti@gmail.com>
2019-11-17 02:39:37 +0200

    Audio player and recorder imporvements and added methods

--- client/positiveloop/src/utils/AudioPlayer.js
+++ client/positiveloop/src/utils/AudioPlayer.js
@@ -1,32 +1,80 @@
 import { Player } from '@react-native-community/audio-toolkit';
 
+var player: Player;
 
 export default class AudioPlayer {
-    player: Player = null;
 
     /**
      * Play file from local storage
-     * @param {string} fileName the audio file name on local storage 
+     * @param {string} fileName the audio file name on local storage
+     * @param {function} callBack when the player is started
      */
-    playFile(fileName) {
-        this.player = new Player(filePath, {
-            continuesToPlayInBackground: false
-        }).play();
+    static playFile(fileName, callBack) {
+        AudioPlayer.startPlayer(fileName, callBack);
     }
 
     /**
      * Play file straight from server 
      * @param {string} fileId the audio file id on server 
+     * @param {function} callBack when the player is started
      */
-    playUrl(fileId) {
-        this.player = new Player("http://54.229.97.181:8080/mental/file/" + fileId, {
-            continuesToPlayInBackground: false
-        }).play();
+    static playUrl(fileId, callBack) {
+        AudioPlayer.startPlayer("http://54.229.97.181:8080/mental/file/" + fileId, callBack);
     }
 
-    /** Stop playing the audio */
-    stop() {
-        if (this.player !== null)
-            this.player.stop();
+    /**
+     * Get relative progress of the player
+     * @returns {number} the current progress between [0, 1]
+     */
+    static getProgress() {
+        if (player.duration <= 0)
+            return 0;
+        return player.currentTime / player.duration;
+    }
+
+    /**
+    * Get relative progress of the player
+    * @returns {number} the current play time in milliseconds
+    */
+    static getTime() {
+        return player.currentTime;
+    }
+
+    /**
+    * Seek to time
+    * @param {number} position [0, 1]
+    * @returns {number} current time in milliseconds
+    */
+    static seek(position) {
+        player.seek(position);
+        return player.currentTime;
+    }
+
+    /**
+    * Seek to relative time / progress
+    * @param {number} position in milliseconds to seek to
+    * @returns {number} current relative time / progress [0, 1]
+    */
+    static seekRelative(position) {
+        player.seek(position);
+        return player.currentTime;
+    }
+
+    /**
+     * Stop playing the audio
+     * @param {function} callBack when the player is stopped
+    */
+    static stop() {
+        if (player !== undefined && player.canStop)
+            player.stop(callBack);
+    }
+
+    static startPlayer(file, callBack) {
+        AudioPlayer.stop();
+        player = new Player(file, {
+            continuesToPlayInBackground: true,
+            wakeLock: true,
+
+        }).play(callBack);
     }
 }
--- client/positiveloop/src/utils/AudioRecorder.js
+++ client/positiveloop/src/utils/AudioRecorder.js
@@ -2,17 +2,14 @@ import { Recorder } from '@react-native-community/audio-toolkit';
 import { Date } from 'core-js';
 import { Alert, PermissionsAndroid } from 'react-native';
 
-export default class AudioRecorder {
-    recorder: Recorder = null;
+var recorder: Recorder;
 
-    constructor() {
-        this.checkPermissions();
-    }
+export default class AudioRecorder {
 
     /** Check if required permissions are granted
      * @returns {boolean} permissions granted?
      */
-    async checkPermissions() {
+    static async checkPermissions() {
         if (Platform.OS === 'android') {
             try {
                 const granted = await PermissionsAndroid.request(
@@ -40,36 +37,41 @@ export default class AudioRecorder {
         }
     }
 
-    getName() {
-        return "recording-" + Date.now() + ".aac";
-    }
+    /**
+     * Start recording audio to a file
+     * @param {function} callback when the player is started
+    */
+    static async record(callBack) {
 
-    /** Start recording audio to a file */
-    async record() {
-        
         // Check permission before trying to record
-        const permission = await this.checkPermissions();
+        const permission = await AudioPlayer.checkPermissions();
         if (!permission)
             return;
 
-        // Recorder initialised, stop (and destroy) before creating new
-        if (this.recorder !== null) {
-            this.stop();
-        }
+        AudioRecorder.stop();
 
         // Create recorder and start recording
-        this.recorder = new Recorder(this.getName(), {
-            bitrate: 25000,
+        recorder = new Recorder(AudioPlayer.getName(), {
+            bitrate: 128000,
             channels: 2,
             sampleRate: 44100,
             quality: 'max'
-        }).record();
+        }).record(callBack);
     }
 
-    /** Stop and get path to saved recording */
-    stop(): string {
-        const filePath = this.recorder.fsPath;
-        this.recorder.stop();
+    /**
+     * Stop and get path to saved recording
+     * @param {function} callback when the player is started
+    */
+    static stop(callBack): string {
+        if (recorder === undefined)
+            return null;
+        const filePath = recorder.fsPath;
+        recorder.stop(callBack);
         return filePath;
+    }
+
+    static getName() {
+        return "recording-" + Date.now() + ".aac";
     }
 }
