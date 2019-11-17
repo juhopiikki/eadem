0e3ead0fd3f3c8ab1340a79a19d79ae0a3ba8e76
jonirautiainen <joni.rautiainen@aalto.fi>
2019-11-17 06:02:54 +0200

    oh

--- client/positiveloop/src/assets/colors.js
+++ client/positiveloop/src/assets/colors.js
@@ -1,6 +1,6 @@
 const colors = {
     inactiveTab: 'rgb(100,100,100)',
-    activeTab: 'rgb(0,0,255)',
+    activeTab: '#5300DA',
     background: 'rgb(255,255,255)',
     buttonDefault: '#5300DA',
 };
--- client/positiveloop/src/screens/Record/index.js
+++ client/positiveloop/src/screens/Record/index.js
@@ -68,10 +68,12 @@ export default Record = () => {
 
     return (
         <>
+        {/* <View style={styles.main}> */}
+            <KeyboardAwareScrollView contentContainerStyle={styles.main} keyboardShouldPersistTaps='handled'>
             <View style={{
               flex: 1,
               flexDirection: 'column', 
-              justifyContent: 'center',
+              justifyContent: 'flex-start',
               alignItems: 'center',
             }}>
 
@@ -301,11 +303,17 @@ export default Record = () => {
                     </Button>
                 </View>
             </View>
+            </KeyboardAwareScrollView>
+        {/* </View> */}
         </>
     );
 };
 
 const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    backgroundColor: colors.background,
+},
   tinyLabel: {
       fontFamily: 'NunitoSans',
       fontSize: 12,
